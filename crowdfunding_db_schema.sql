a65d87c6333f56a41b331af5bb86500155b1696d
Freya Huang <yh.freya@gmail.com>
2024-08-13 20:40:24 +1000

    Added DataBase by Yuqi Huang

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,45 @@
+﻿CREATE TABLE category (
+    category_id VARCHAR(5) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    PRIMARY KEY (category_id)
+);
+
+CREATE TABLE subcategory (
+    subcategory_id VARCHAR(25) NOT NULL,
+    subcategory VARCHAR(255) NOT NULL,
+    PRIMARY KEY (subcategory_id)
+);
+
+CREATE TABLE contacts (
+    contact_id INT NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (contact_id)
+);
+
+CREATE TABLE campaigns (
+    cf_id INT NOT NULL,
+    contact_id INT NOT NULL,
+    company_name VARCHAR(255) NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    goal INT NOT NULL,
+    pledged INT NOT NULL,
+    outcome VARCHAR(25) NOT NULL, 
+    backers_count INT NOT NULL,
+    country CHAR(2) NOT NULL,
+    currency CHAR(3) NOT NULL,
+    launch_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    category_id VARCHAR(5) NOT NULL,
+    subcategory_id VARCHAR(25) NOT NULL,
+    PRIMARY KEY (cf_id),
+    FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+    FOREIGN KEY (category_id) REFERENCES category(category_id),
+    FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+);
+
+SELECT * FROM category limit(10)
+SELECT * FROM subcategory limit(10)
+SELECT * FROM contacts limit(10)
+SELECT * FROM campaigns limit(10)
